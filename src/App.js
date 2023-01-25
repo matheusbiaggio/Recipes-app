28d5c99f625482a3ef4989987ff0895d6f91f676
Matheus Biaggio <matheusbiaggio@gmail.com>
2023-01-25 17:37:10 -0300

    realização dos requisitos 2-6

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import Meals from './pages/Meals';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        component={ Login }
+      />
+      <Route
+        exact
+        path="/meals"
+        component={ Meals }
+      />
+      <Route path="*" component={ PageNotFound } exact />
+    </Switch>
   );
 }
 
---
+++ src/tests/Login.test.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { screen, userEvent } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
+import renderWithRouter from './helpers/renderWithRouter';
+
+const EMAIL_CONST = 'email@email.com';
+const PASSWORD_CONST = 'password';
+
+describe('Teste para a tela de login', () => {
+  test('Testa se renderiza o componente login', () => {
+    renderWithRouter(<App />);
+
+    const title = screen.getByRole('heading', {
+      name: /login/i,
+      level: 1,
+    });
+
+    expect(title).toBeDefined();
+  });
+
+  test('Testa se existe os inputs de email e senha e o botão', () => {
+    renderWithRouter(<App />);
+
+    const loginInput = screen.getByRole('textbox');
+    const passwordInput = screen.getByPlaceholderText(/password/i);
+    const btnEnter = screen.getByRole('button', {
+      name: /enter/i,
+    });
+
+    expect(loginInput).toBeDefined();
+    expect(passwordInput).toBeDefined();
+    expect(btnEnter).toBeDefined();
+  });
+
+  test('Testa se o botão inicia desabilitado', () => {
+    renderWithRouter(<App />);
+
+    const btnEnter = screen.getByRole('button', {
+      name: /enter/i,
+    });
+
+    expect(btnEnter).toBeDisabled();
+  });
+
+  test('Testa se o botão fica habilidato ao preencher corretamente e direciona para a pagina "Meals"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const loginInput = screen.getByRole('textbox');
+    const passwordInput = screen.getByPlaceholderText(/password/i);
+    const btnEnter = screen.getByRole('button', {
+      name: /enter/i,
+    });
+
+    console.log(loginInput);
+
+    userEvent.type(loginInput, EMAIL_CONST);
+    userEvent.type(passwordInput, PASSWORD_CONST);
+
+    expect(btnEnter).toBeEnabled();
+
+    userEvent.click(btnEnter);
+
+    expect(history.location.pathname).toBe('/meals');
+
+    // act(() => {
+    //   history.push('/meals');
+    // });
+  });
+});
