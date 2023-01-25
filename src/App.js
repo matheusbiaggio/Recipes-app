098750be483c0a38e375d8d88e9d60acf4d7e33a
pedrojaoa <joaopfacbc@gmail.com>
2023-01-25 18:13:53 -0300

    Requisito 7 [ROTAS E HEADER] em desenvolvimento...

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import HeaderProvider from './context/HeaderProvider';
+
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+    <div>
+      <Switch>
+        <HeaderProvider>
+          <Route exact path="/" component={ Header } />
+          {/* <Route exact path="/meals" component={ Meals } />
+          <Route exact path="/drinks" component={ Drinks } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/done-recipes" component={ DoneRecipes } />
+          <Route exact path="/favorite-recipes" component={ FavoriteRecipes } /> */}
+        </HeaderProvider>
+        {/*
+        <Route exact path="/meals/:id-da-receita" component={ Meals } />
+        <Route exact path="/meals/:id-da-receita/in-progress" component={ Meals } />
+        <Route exact path="/drinks/:id-da-receita" component={ Drinks } />
+        <Route exact path="/drinks/:id-da-receita/in-progress" component={ Drinks } /> */}
+      </Switch>
     </div>
   );
 }
