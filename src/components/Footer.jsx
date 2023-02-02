5b8aa27c59b083503b03c529200b4d3a96396421
Matheus Biaggio <matheusbiaggio@gmail.com>
2023-02-02 18:03:53 -0300

    arrumar a rota

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import '../Css/CssFooter.css';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 export default function Footer() {
   const history = useHistory();
 
   return (
     <div data-testid="footer" className="Footer">
-      <button
-        src="src/imagens/drinkIcon.svg"
+      <img
+        src={ drinkIcon }
+        alt="Dinks"
         data-testid="drinks-bottom-btn"
         onClick={ () => history.push('/drinks') }
-      >
-        Drinks
-      </button>
-      <button
-        src="src/imagens/mealIcon.svg"
+        aria-hidden="true"
+      />
+      <img
+        src={ mealIcon }
+        alt="Meals"
         data-testid="meals-bottom-btn"
         onClick={ () => history.push('/meals') }
-      >
-        Meals
-      </button>
+        aria-hidden="true"
+      />
     </div>
   );
 }
