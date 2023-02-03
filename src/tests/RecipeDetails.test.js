4c2eb9e6a7e907f4df576275e10b93c94c89bcf3
pedrojaoa <joaopfacbc@gmail.com>
2023-02-03 17:40:06 -0300

    RecipeDetails testes em 50%

--- src/tests/RecipeDetails.test.js
+++ src/tests/RecipeDetails.test.js
@@ -1,4 +1,3 @@
-// import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import { fireEvent, screen } from '@testing-library/react';
 import renderWithRouter from './helpers/renderWithRouter';
@@ -6,11 +5,15 @@ import App from '../App';
 
 const RECIPE_0_ID = '0-recipe-card';
 const RECIPE_TITLE_ID = 'recipe-title';
-const FAVORITE_BUTTON = 'favorite-btn';
 const SHARE_BUTTON = 'share-btn';
+const INGREDIENT_NAME_0 = '0-ingredient-name-and-measure';
+const START_RECIPE_BUTTON = 'start-recipe-btn';
+const RECIPE_CATEGORY_ID = 'recipe-category';
+const RECIPE_INSTRUCTION_ID = 'instructions';
+const RECOMMENDATION_0_ID = '0-recommendation-card';
 
 describe('Testando o componente "RecipeDetails:"', () => {
-  test('Testa se renderiza os detalhes corretamente:', async () => {
+  test('Testa se renderiza os detalhes corretamente [MEALS]:', async () => {
     const { history } = renderWithRouter(<App />);
     act(() => {
       history.push('/meals');
@@ -21,9 +24,75 @@ describe('Testando o componente "RecipeDetails:"', () => {
 
     const recipeTitle = screen.findByTestId(RECIPE_TITLE_ID);
     expect(recipeTitle).toBeDefined();
-    const favoriteIcon = await screen.findByAltText('whiteHeart');
+
+    const favoriteIcon = await screen.findByAltText('whiteHeartIcon');
     expect(favoriteIcon).toBeDefined();
+
     const shareBtn = await screen.findByTestId(SHARE_BUTTON);
+    expect(shareBtn).toBeDefined();
+
+    const ingredientZero = await screen.findByTestId(INGREDIENT_NAME_0);
+    expect(ingredientZero).toBeDefined();
+
+    const recipeCategory = await screen.findByTestId(RECIPE_CATEGORY_ID);
+    expect(recipeCategory).toBeDefined();
+
+    const instructions = await screen.findByTestId(RECIPE_INSTRUCTION_ID);
+    expect(instructions).toBeDefined();
+
+    const recommendation = await screen.findByTestId(RECOMMENDATION_0_ID);
+    expect(recommendation).toBeDefined();
+
+    const startButton = await screen.findByTestId(START_RECIPE_BUTTON);
+    expect(startButton).toBeDefined();
+    fireEvent.click(startButton);
+    expect(history.location.pathname).toBe('/meals/52977/in-progress');
+  });
+  test('Testa se renderiza os detalhes corretamente [DRINKS]:', async () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/drinks');
+    });
+    const recipeZero = await screen.findByTestId(RECIPE_0_ID);
+    expect(recipeZero).toBeDefined();
+    fireEvent.click(recipeZero);
+
+    const recipeTitle = screen.findByTestId(RECIPE_TITLE_ID);
+    expect(recipeTitle).toBeDefined();
+
+    const favoriteIcon = await screen.findByAltText('whiteHeartIcon');
+    expect(favoriteIcon).toBeDefined();
+
+    const shareBtn = await screen.findByTestId(SHARE_BUTTON);
+    expect(shareBtn).toBeDefined();
+
+    const ingredientZero = await screen.findByTestId(INGREDIENT_NAME_0);
+    expect(ingredientZero).toBeDefined();
+
+    const recipeCategory = await screen.findByTestId(RECIPE_CATEGORY_ID);
+    expect(recipeCategory).toBeDefined();
+
+    const instructions = await screen.findByTestId(RECIPE_INSTRUCTION_ID);
+    expect(instructions).toBeDefined();
+
+    const recommendation = await screen.findByTestId(RECOMMENDATION_0_ID);
+    expect(recommendation).toBeDefined();
+
+    const startButton = await screen.findByTestId(START_RECIPE_BUTTON);
+    expect(startButton).toBeDefined();
+    fireEvent.click(startButton);
+    expect(history.location.pathname).toBe('/drinks/15997/in-progress');
+  });
+  test('Testa as funções do componente', async () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/meals');
+    });
+    const recipeZero = await screen.findByTestId(RECIPE_0_ID);
+    expect(recipeZero).toBeDefined();
+    fireEvent.click(recipeZero);
+
+    const shareBtn = screen.getByTestId(SHARE_BUTTON);
     expect(shareBtn).toBeDefined();
   });
 });
