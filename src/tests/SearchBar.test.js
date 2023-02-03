0939b2efddceded93adc95ed526bab375fcc2e1c
pedrojaoa <joaopfacbc@gmail.com>
2023-02-03 15:15:53 -0300

    Testes do searchBar

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
-import fetch from '../../cypress/mocks/fetch';
+import drinks from '../../cypress/mocks/drinks';
+import meals from '../../cypress/mocks/meals';
 
 const SEARCH_TOP_BTN = 'search-top-btn';
 const SEARCH_INPUT = 'search-input';
@@ -14,22 +15,18 @@ const FIRST_LETTER_SEARCH_RADIO = 'first-letter-search-radio';
 const EXEC_SEARCH_BTN = 'exec-search-btn';
 
 describe('Testando o componente "SearchBar":', () => {
-  beforeEach(async () => {
-    global.fetch = jest.fn(fetch);
-  });
-  test('Teste se os elementos estao renderizados:', async () => {
+  test('Teste se os elementos de meals estao renderizados:', async () => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(meals),
+    });
     const { history } = renderWithRouter(<App />);
-    await act(() => history.push('/meals'));
+    await act(async () => history.push('/meals'));
     const alertMock = jest.spyOn(global, 'alert').mockImplementation();
-
     const searchBtn = screen.getByTestId(SEARCH_TOP_BTN);
     expect(searchBtn).toBeDefined();
-
     userEvent.click(searchBtn);
-
     const textbox = screen.getByTestId(SEARCH_INPUT);
     expect(textbox).toBeDefined();
-
     const ingredientRadio = screen.getByTestId(INGREDIENT_SEARCH_RADIO);
     const nameRadio = screen.getByTestId(NAME_SEARCH_RADIO);
     const firstLetterRadio = screen.getByTestId(FIRST_LETTER_SEARCH_RADIO);
@@ -38,77 +35,93 @@ describe('Testando o componente "SearchBar":', () => {
     expect(nameRadio).toBeDefined();
     expect(firstLetterRadio).toBeDefined();
     expect(execSearch).toBeDefined();
-
-    await act(() => {
+    await act(async () => {
       userEvent.click(firstLetterRadio);
       userEvent.type(textbox, 'aa');
+      userEvent.click(execSearch);
     });
-    expect(alertMock).toHaveBeenCalled();
+    await waitFor(() => {
+      expect(alertMock).toHaveBeenCalled();
+    });
   });
   test('Testa se a url foi chamada corretamente digitando "Chicken" no input de search', async () => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(meals),
+    });
     const { history } = renderWithRouter(<App />);
-    await act(() => history.push('/meals'));
-    const fetch1 = jest.spyOn(global, 'fetch');
-    fetch1.mockImplementation(fetch);
+    await act(async () => history.push('/meals'));
     const searchBtn = screen.getByTestId(SEARCH_TOP_BTN);
-    userEvent.click(searchBtn);
-    const textbox = screen.getByTestId(SEARCH_INPUT);
-    userEvent.type(textbox, 'Chicken');
+    await act(async () => {
+      userEvent.click(searchBtn);
+    });
+    const textbox = await screen.findByTestId(SEARCH_INPUT);
     const ingredientRadio = screen.getByTestId(INGREDIENT_SEARCH_RADIO);
-    userEvent.click(ingredientRadio);
     const execSearch = screen.getByTestId(EXEC_SEARCH_BTN);
-    userEvent.click(execSearch);
-
+    await act(async () => {
+      userEvent.type(textbox, 'Chicken');
+      userEvent.click(ingredientRadio);
+      userEvent.click(execSearch);
+    });
     expect(global.fetch).toHaveBeenCalledWith('https://www.themealdb.com/api/json/v1/1/filter.php?i=Chicken');
   });
   test('Testa se a url foi chamada corretamente digitando "Light rum" no input de search', async () => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(drinks),
+    });
     const { history } = renderWithRouter(<App />);
-    await act(() => history.push('/drinks'));
-    const fetch1 = jest.spyOn(global, 'fetch');
-    fetch1.mockImplementation(fetch);
+    await act(async () => history.push('/drinks'));
     const searchBtn = screen.getByTestId(SEARCH_TOP_BTN);
-    userEvent.click(searchBtn);
-    const textbox = screen.getByTestId(SEARCH_INPUT);
-    userEvent.type(textbox, 'Light rum');
+    await act(async () => {
+      userEvent.click(searchBtn);
+    });
+    const textbox = await screen.findByTestId(SEARCH_INPUT);
     const ingredientRadio = screen.getByTestId(INGREDIENT_SEARCH_RADIO);
-    userEvent.click(ingredientRadio);
     const execSearch = screen.getByTestId(EXEC_SEARCH_BTN);
-    userEvent.click(execSearch);
-
+    await act(async () => {
+      userEvent.type(textbox, 'Light rum');
+      userEvent.click(ingredientRadio);
+      userEvent.click(execSearch);
+    });
     expect(global.fetch).toHaveBeenCalledWith('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Light rum');
   });
   test('Testa se a url foi chamada corretamente digitando "Arrabiata" no input de search', async () => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(meals),
+    });
     const { history } = renderWithRouter(<App />);
-    await act(() => history.push('/meals'));
-    const fetch1 = jest.spyOn(global, 'fetch');
-    fetch1.mockImplementation(fetch);
+    await act(async () => history.push('/meals'));
     const searchBtn = screen.getByTestId(SEARCH_TOP_BTN);
-    userEvent.click(searchBtn);
-    const textbox = screen.getByTestId(SEARCH_INPUT);
-    userEvent.type(textbox, 'Arrabiata');
+    await act(async () => {
+      userEvent.click(searchBtn);
+    });
+    const textbox = await screen.findByTestId(SEARCH_INPUT);
     const nameRadio = screen.getByTestId(NAME_SEARCH_RADIO);
-    userEvent.click(nameRadio);
     const execSearch = screen.getByTestId(EXEC_SEARCH_BTN);
-    userEvent.click(execSearch);
-
+    await act(async () => {
+      userEvent.type(textbox, 'Arrabiata');
+      userEvent.click(nameRadio);
+      userEvent.click(execSearch);
+    });
     expect(global.fetch).toHaveBeenCalledWith('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata');
   });
   test('Testa se a url foi chamada corretamente digitando "Aquamarine" no input de search', async () => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(drinks),
+    });
     const { history } = renderWithRouter(<App />);
-    await act(() => history.push('/drinks'));
-    const fetch4 = jest.spyOn(global, 'fetch');
-    fetch4.mockImplementation(fetch);
+    await act(async () => history.push('/drinks'));
     const searchBtn = screen.getByTestId(SEARCH_TOP_BTN);
-    userEvent.click(searchBtn);
-    const textbox = screen.getByTestId(SEARCH_INPUT);
-    userEvent.type(textbox, 'Aquamarine');
+    await act(async () => {
+      userEvent.click(searchBtn);
+    });
+    const textbox = await screen.findByTestId(SEARCH_INPUT);
     const nameRadio = screen.getByTestId(NAME_SEARCH_RADIO);
-    userEvent.click(nameRadio);
     const execSearch = screen.getByTestId(EXEC_SEARCH_BTN);
-    userEvent.click(execSearch);
-
+    await act(async () => {
+      userEvent.type(textbox, 'Aquamarine');
+      userEvent.click(nameRadio);
+      userEvent.click(execSearch);
+    });
     expect(global.fetch).toHaveBeenCalledWith('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Aquamarine');
-  });
-  test('Testa se a url foi chamada corretamente', async () => {
   });
 });
